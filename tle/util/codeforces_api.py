c579b70818f72cc92d3b7dc707b18b54b0b4375b
Denjell <73618585+Denjell@users.noreply.github.com>
2026-06-22 23:12:15 +0200

    Respecting contest.standings only allowing contestId parameter

--- tle/util/codeforces_api.py
+++ tle/util/codeforces_api.py
@@ -427,16 +427,23 @@ class contest:
         show_unofficial: Optional[bool] = None,
     ) -> Tuple[Contest, List[Problem], List[RanklistRow]]:
         params = {'contestId': contest_id}
+        ## Comment (denjell): Current API does not allow for any other param than contestId
+        
         if from_ is not None:
-            params['from'] = from_
+            logger.error(f'contest.standings does not allow params other than contest_id, got from')
+            # params['from'] = from_
         if count is not None:
-            params['count'] = count
+            logger.error(f'contest.standings does not allow params other than contest_id, got count')
+            # params['count'] = count
         if handles is not None:
-            params['handles'] = ';'.join(handles)
+            logger.error(f'contest.standings does not allow params other than contest_id, got handles')
+            # params['handles'] = ';'.join(handles)
         if room is not None:
-            params['room'] = room
+            logger.error(f'contest.standings does not allow params other than contest_id, got room')
+            # params['room'] = room
         if show_unofficial is not None:
-            params['showUnofficial'] = _bool_to_str(show_unofficial)
+            logger.error(f'contest.standings does not allow params other than contest_id, got showUnofficial')
+            # params['showUnofficial'] = _bool_to_str(show_unofficial)
         try:
             resp = await _query_api('contest.standings', params)
         except TrueApiError as e:
